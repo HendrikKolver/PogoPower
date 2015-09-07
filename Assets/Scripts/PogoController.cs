e4a6ecd1c5bf7d71ba299c1cc92383936b819479
Hendrik Kolver <hendrik.kolver@gmail.com>
2015-09-07 19:47:52 +0200

    Touch inputs working well

--- Assets/Scripts/PogoController.cs
+++ Assets/Scripts/PogoController.cs
@@ -23,95 +23,101 @@ public class PogoController : MonoBehaviour {
 	private Vector2 spawnPoint;
 	private bool canJump;
 
-	private static PogoController thisObject;
-	private static Collider thisGameCollider;
-
-	
 	void Awake()
 	{
 		// Setting up references.
 		groundDetector = transform.Find("GroundDetector");
 		spawnPoint = transform.position;
-		thisObject = this;
-		thisGameCollider = transform.GetComponent<Collider>();
-	}
-	
-	void Update()
-	{
-		float yOfset = 0.47f;
-		float xOfset = 0.5f;
-		Vector2 startLineCast = new Vector2 (transform.position.x-xOfset,transform.position.y-yOfset);
-		Vector2 endLineCast = new Vector2 (transform.position.x+xOfset,transform.position.y-yOfset);
-		grounded = Physics2D.Linecast(startLineCast, endLineCast, 1 << LayerMask.NameToLayer("Ground"));
-
-		if (grounded) {
-			bounce = false;
-		}
-
-		if (jumpPressed && grounded && !bounce) {
-			jump = true;
-			jumpPressed = false;
-
-			//Reset the velocity to ensure that jump height is always equal
-			GetComponent<Rigidbody2D>().velocity= new Vector2(GetComponent<Rigidbody2D>().velocity.x,0f);
-		}
-
-
-		if(Input.GetButtonDown("Jump"))
-			jumpPressed = true;
-
-		if (grounded) {
-			randomMovementCounter++;
-		}
-
-		if (randomMovementCounter >= randomMovementTimer && grounded && !jump && !jumpPressed) {
-			bounce = true;
-			moveObject(GetComponent<Rigidbody2D>(),(Vector2.right * getRandomNumber(-100.0f,100.0f)));
-			if(GetComponent<Rigidbody2D>().velocity.y<=0f) 
-				jumpObject(GetComponent<Rigidbody2D>(),(jumpForce/1.5f));
-			randomMovementCounter = 0;
-		}
-
-		if(isObjectTooLow(GetComponent<Transform>())){
-			returnToSpawnPoint();
-		}
 	}
 	
 	void FixedUpdate ()
 	{
-		// Cache the horizontal input.
-		//float h = Input.GetAxis("Horizontal");
-		float h = 0f;
-		if (Input.touchCount > 0)
-		{
-			Touch[] touches = Input.touches;
-			foreach(Touch touch in touches){
-				if (touch.position.x < Screen.width/4)
-				{
-					h = -1f;
-				}
-				else if (touch.position.x > Screen.width - Screen.width/4)
-				{
-					h = 1f;
-				}else{
-					jumpPressed = true;
-				}
-			}
+		grounded = isGrounded ();
+		if (grounded) {
+			bounce = false;
 		}
 		
-		if (h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed) {
-			moveObject(GetComponent<Rigidbody2D>(),(Vector2.right * h * moveForce)); 
-		}		 		
-		if (Mathf.Abs (GetComponent<Rigidbody2D>().velocity.x) > maxSpeed) {
-			GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		if (jumpPressed && grounded && !bounce) {
+			jump = true;
+			jumpPressed = false;
+			
+			//Reset the velocity to ensure that jump height is always equal
+			GetComponent<Rigidbody2D>().velocity= new Vector2(GetComponent<Rigidbody2D>().velocity.x,0f);
 		}
 
-		if(jump)
-		{	
-			jumpObject(GetComponent<Rigidbody2D>(),jumpForce);
-			moveObject(GetComponent<Rigidbody2D>(),(Vector2.right * getRandomNumber(-100.0f,100.0f)));
+		addRandomMovement ();
+
+		//Check if player in bounds
+		if(isObjectTooLow(GetComponent<Transform>())){
+			returnToSpawnPoint();
+		}
+
+		var h = getTouchInput ();
+		applyMovement (h);		 		
+		jumpIfNeeded ();
+	}
+
+	void jumpIfNeeded ()
+	{
+		if (jump) {
+			jumpObject (GetComponent<Rigidbody2D> (), jumpForce);
+			moveObject (GetComponent<Rigidbody2D> (), (Vector2.right * getRandomNumber (-100.0f, 100.0f)));
 			jump = false;
 		}
+	}
+
+	void applyMovement (float h)
+	{
+		if (h * GetComponent<Rigidbody2D> ().velocity.x < maxSpeed) {
+			moveObject (GetComponent<Rigidbody2D> (), (Vector2.right * h * moveForce));
+		}
+		if (Mathf.Abs (GetComponent<Rigidbody2D> ().velocity.x) > maxSpeed) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (Mathf.Sign (GetComponent<Rigidbody2D> ().velocity.x) * maxSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		}
+	}
+
+	float getTouchInput ()
+	{
+		float h = 0f;
+		if (Input.touchCount > 0) {
+			Touch[] touches = Input.touches;
+			foreach (Touch touch in touches) {
+				if (touch.position.x < Screen.width / 4) {
+					h = -1f;
+				}
+				else
+					if (touch.position.x > Screen.width - Screen.width / 4) {
+						h = 1f;
+					}
+					else {
+						jumpPressed = true;
+					}
+			}
+		}
+		return h;
+	}
+
+	void addRandomMovement ()
+	{
+		if (grounded) {
+			randomMovementCounter++;
+		}
+		if (randomMovementCounter >= randomMovementTimer && grounded && !jump && !jumpPressed) {
+			bounce = true;
+			moveObject (GetComponent<Rigidbody2D> (), (Vector2.right * getRandomNumber (-100.0f, 100.0f)));
+			if (GetComponent<Rigidbody2D> ().velocity.y <= 0f)
+				jumpObject (GetComponent<Rigidbody2D> (), (jumpForce / 1.5f));
+			randomMovementCounter = 0;
+		}
+	}
+
+	bool isGrounded ()
+	{
+		float yOfset = 0.47f;
+		float xOfset = 0.5f;
+		Vector2 startLineCast = new Vector2 (transform.position.x - xOfset, transform.position.y - yOfset);
+		Vector2 endLineCast = new Vector2 (transform.position.x + xOfset, transform.position.y - yOfset);
+		return Physics2D.Linecast (startLineCast, endLineCast, 1 << LayerMask.NameToLayer ("Ground"));
 	}
 
 	private void moveObject(Rigidbody2D component, Vector2 force){
